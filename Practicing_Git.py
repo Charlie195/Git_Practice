a13bcac10b240b7beb528350781ca1c7ff86065d
Charlie Li <charlie.li711935@gmail.com>
2021-09-01 13:52:46 -0400

    Initial commit.
    This is our first commit.

---
+++ Practicing_Git.py
@@ -0,0 +1,36 @@
+print("PRACTICE GIT")
+# TODO: Fix Bug
+name = "Jamil"
+name2 = "Jamila"
+
+def hello(name: str):
+    print(name.upper())
+
+# Alt + Ins to generate code
+
+test = "yo"
+
+# Ctrl + Alt + v to generate variable (refactor code)
+
+hey = "hey"
+print(hey)
+
+# Ctrl + Alt + m to generate method (refactor code)
+
+bro = "bro"
+print("table")
+
+# def table():
+#     bro = "bro"
+#     print("table")
+#
+#
+# table()
+
+# Shift + F6 to rename code occurences (everything: classes, methods, variables, files)
+
+# Ctrl + f, then Ctrl + r to find and replace things
+
+test2 = "ni hao"
+
+print(test2)
